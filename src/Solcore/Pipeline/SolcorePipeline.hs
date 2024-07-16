c83ee8bf95d31dc62a2d53af655824b4a3e8f7b5
Marcin Benke <marcin@benke.org>
2024-07-16 09:57:17 +0200

    Pipeline: add verbose option

--- src/Solcore/Pipeline/SolcorePipeline.hs
+++ src/Solcore/Pipeline/SolcorePipeline.hs
@@ -2,9 +2,9 @@ module Solcore.Pipeline.SolcorePipeline where
 
 import Control.Monad
 
-import Options.Applicative 
+import Options.Applicative
 
-import Solcore.Desugarer.MatchCompiler 
+import Solcore.Desugarer.MatchCompiler
 import Solcore.Frontend.Lexer.SolcoreLexer
 import Solcore.Frontend.Parser.SolcoreParser
 import Solcore.Frontend.Pretty.SolcorePretty
@@ -13,47 +13,54 @@ import Solcore.Frontend.TypeInference.TcContract
 import Solcore.Frontend.TypeInference.TcEnv
 import Solcore.Desugarer.Specialise
 
--- main compiler driver function 
+-- main compiler driver function
 
-pipeline :: IO () 
-pipeline = do 
+pipeline :: IO ()
+pipeline = do
   opts <- argumentsParser
   content <- readFile (fileName opts)
-  let r1 = runAlex content parser 
-  withErr r1 $ \ ast -> do 
-    r2 <- sccAnalysis ast 
-    withErr r2 $ \ ast' -> do 
-      r3 <- typeInfer ast' 
-      withErr r3 $ \ (c', env) -> do 
+  let r1 = runAlex content parser
+  withErr r1 $ \ ast -> do
+    r2 <- sccAnalysis ast
+    withErr r2 $ \ ast' -> do
+      r3 <- typeInfer ast'
+      withErr r3 $ \ (c', env) -> do
         when (enableLog env) (mapM_ putStrLn (reverse $ logs env))
-        r4 <- matchCompiler c' 
-        withErr r4 $ \ res -> do 
+        r4 <- matchCompiler c'
+        withErr r4 $ \ res -> do
+          when (optVerbose opts) do
+            putStrLn "Desugared contract:"
+            putStrLn (pretty res)
           r5 <- specialiseCompUnit res env
           putStrLn "Specialised contract:"
           putStrLn (pretty r5)
           return ()
-withErr :: Either String a -> (a -> IO ()) -> IO () 
+withErr :: Either String a -> (a -> IO ()) -> IO ()
 withErr r f = either putStrLn f r
 
--- parsing command line arguments 
+-- parsing command line arguments
 
-data Option 
-  = Option {
-      fileName :: FilePath
+data Option
+  = Option
+    { fileName :: FilePath
+    , optVerbose :: !Bool
     } deriving (Eq, Show)
 
-options :: Parser Option 
-options 
+options :: Parser Option
+options
   = Option <$> strOption (
                   long "file"
                <> short 'f'
                <> metavar "FILE"
                <> help "Input file name")
+           <*> switch ( long "verbose"
+               <> short 'v'
+               <> help "Verbose output" )
 
-argumentsParser :: IO Option 
-argumentsParser = do 
+argumentsParser :: IO Option
+argumentsParser = do
   let opts = info (options <**> helper)
-                  (fullDesc <> 
+                  (fullDesc <>
                    header "Solcore - solidity core language")
   opt <- execParser opts
-  return opt 
+  return opt

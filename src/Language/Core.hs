441ff9a7491578335b22120329496c78c7b65626
Marcin Benke <marcin@benke.org>
2024-07-26 10:25:25 +0200

    rename pretty to ppr in Language.Yul,Language.Core and Common.Pretty

--- src/Language/Core.hs
+++ src/Language/Core.hs
@@ -25,7 +25,7 @@ data Expr
     | ECall Name [Expr]
     | EUnit
 instance Show Expr where
-    show = render . pretty
+    show = render . ppr
 
 pattern SAV :: Name -> Expr -> Stmt
 pattern SAV x e = SAssign (EVar x) e
@@ -43,53 +43,53 @@ data Stmt
     -- deriving Show
 
 data Arg = TArg Name Type
-instance Show Stmt where show = render . pretty
+instance Show Stmt where show = render . ppr
 
 data Alt = Alt Name Stmt
 
 newtype Core = Core [Stmt]
-instance Show Core where show = render . pretty
+instance Show Core where show = render . ppr
 
 instance Pretty Type where
-    pretty TWord = text "word"
-    pretty TBool = text "bool"
-    pretty TUnit = text "unit"
-    pretty (TPair t1 t2) = parens (pretty t1 <+> text "*" <+> pretty t2)
-    pretty (TSum t1 t2) = parens (pretty t1 <+> text "+" <+> pretty t2)
-    pretty (TFun ts t) = parens (hsep (map pretty ts) <+> text "->" <+> pretty t)
+    ppr TWord = text "word"
+    ppr TBool = text "bool"
+    ppr TUnit = text "unit"
+    ppr (TPair t1 t2) = parens (ppr t1 <+> text "*" <+> ppr t2)
+    ppr (TSum t1 t2) = parens (ppr t1 <+> text "+" <+> ppr t2)
+    ppr (TFun ts t) = parens (hsep (map ppr ts) <+> text "->" <+> ppr t)
 
 instance Pretty Expr where
-    pretty (EWord i) = text (show i)
-    pretty (EBool b) = text (show b)
-    pretty EUnit = text "()"
-    pretty (EVar x) = text x
-    pretty (EPair e1 e2) = parens (pretty e1 >< comma <+> pretty e2)
-    pretty (EFst e) = text "fst" >< parens (pretty e)
-    pretty (ESnd e) = text "snd" >< parens (pretty e)
-    pretty (EInl e) = text "inl" >< parens (pretty e)
-    pretty (EInr e) = text "inr" >< parens (pretty e)
-    pretty (ECall f es) = text f >< parens(hsep (punctuate comma (map pretty es)))
+    ppr (EWord i) = text (show i)
+    ppr (EBool b) = text (show b)
+    ppr EUnit = text "()"
+    ppr (EVar x) = text x
+    ppr (EPair e1 e2) = parens (ppr e1 >< comma <+> ppr e2)
+    ppr (EFst e) = text "fst" >< parens (ppr e)
+    ppr (ESnd e) = text "snd" >< parens (ppr e)
+    ppr (EInl e) = text "inl" >< parens (ppr e)
+    ppr (EInr e) = text "inr" >< parens (ppr e)
+    ppr (ECall f es) = text f >< parens(hsep (punctuate comma (map ppr es)))
 
 instance Pretty Stmt where
-    pretty (SAssign lhs rhs) = pretty lhs <+> text ":=" <+> pretty rhs
-    pretty (SAlloc x t) = text "let" <+> text x <+> text ":" <+> pretty t
-    pretty (SExpr e) = pretty e
-    pretty (SAssembly stmts) = vcat (map pretty stmts)
-    pretty (SReturn e) = text "return" <+> pretty e
-    pretty (SComment c) = text "//" <+> text c
-    pretty (SBlock stmts) = lbrace $$ nest 4 (vcat (map pretty stmts)) $$ rbrace
-    pretty (SMatch e [left, right]) =
-        text "match" <+> pretty e <+> text "with" $$ lbrace
+    ppr (SAssign lhs rhs) = ppr lhs <+> text ":=" <+> ppr rhs
+    ppr (SAlloc x t) = text "let" <+> text x <+> text ":" <+> ppr t
+    ppr (SExpr e) = ppr e
+    ppr (SAssembly stmts) = vcat (map ppr stmts)
+    ppr (SReturn e) = text "return" <+> ppr e
+    ppr (SComment c) = text "//" <+> text c
+    ppr (SBlock stmts) = lbrace $$ nest 4 (vcat (map ppr stmts)) $$ rbrace
+    ppr (SMatch e [left, right]) =
+        text "match" <+> ppr e <+> text "with" $$ lbrace
            $$ nest 2 (vcat [prettyAlt "inl" left, prettyAlt "inr" right]) $$ rbrace
         where
-            prettyAlt tag (Alt n s) = text tag <+> text n <+> text "=>" <+> pretty s
-    pretty (SFunction f args ret stmts) =
-        text "function" <+> text f <+> parens (hsep (punctuate comma (map pretty args))) <+> text "->" <+> pretty ret <+> lbrace
-           $$ nest 2 (vcat (map pretty stmts))  $$ rbrace
-    pretty (SRevert s) = text "revert" <+> text s
+            prettyAlt tag (Alt n s) = text tag <+> text n <+> text "=>" <+> ppr s
+    ppr (SFunction f args ret stmts) =
+        text "function" <+> text f <+> parens (hsep (punctuate comma (map ppr args))) <+> text "->" <+> ppr ret <+> lbrace
+           $$ nest 2 (vcat (map ppr stmts))  $$ rbrace
+    ppr (SRevert s) = text "revert" <+> text s
 
 instance Pretty Arg where
-    pretty (TArg n t) = text n <+> text ":" <+> pretty t
+    ppr (TArg n t) = text n <+> text ":" <+> ppr t
 
 instance Pretty Core where
-    pretty (Core stmts) = vcat (map pretty stmts)
+    ppr (Core stmts) = vcat (map ppr stmts)
